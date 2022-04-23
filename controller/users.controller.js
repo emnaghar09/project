ee56bd3dfecc3c31c85a129836b1e5ee1c419c65
emna <emna.ghariani09@gmail.com>
2022-04-23 11:19:28 +0100

    make it better

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require ('jsonwebtoken')
 const userSchema=require('../model/users.model')
 const roleSchema = require('../model/role.model')
